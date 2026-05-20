836ee1add636ab70e0e74e7ab34b60970c886c3d
Momster <bigbob12352@outlook.com>
2026-05-20 21:57:59 +0800

    Update NotificationManager.h

--- src/NotificationManager.h
+++ src/NotificationManager.h
@@ -5,6 +5,7 @@
 #include <QWidget>
 #include <QTimer>
 #include <QMediaPlayer>
+#include <QPointer>
 
 class NotificationManager : public QObject {
     Q_OBJECT
@@ -12,16 +13,18 @@ public:
     static NotificationManager& instance();
     void configure(bool enabled, int durationMs, bool soundEnabled);
     void showMessage(const QString& text);
+    void playSound();
 
 private:
     NotificationManager();
     ~NotificationManager();
-    QWidget* m_window = nullptr;
+    QPointer<QWidget> m_window;
     QTimer m_hideTimer;
     QMediaPlayer* m_player = nullptr;
     bool m_enabled = true;
     int m_duration = 1000;
     bool m_sound = true;
+    bool m_soundAvailable = false;
 };
 
 #endif // NOTIFICATIONMANAGER_H
