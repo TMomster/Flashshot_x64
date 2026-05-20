20e1440639a32b89f70322a135ba296442674ac5
Momster <bigbob12352@outlook.com>
2026-05-20 20:02:39 +0800

    Create MainWindow.h

---
+++ src/MainWindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSystemTrayIcon>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+protected:
+    void closeEvent(QCloseEvent* event) override;
+
+private slots:
+    void onHotkeyTriggered(const QString& id);
+    void onTrayActivated(QSystemTrayIcon::ActivationReason reason);
+    void toggleReplay();
+    void openSaveDir();
+    void openLogDir();
+    void runWizard();
+    void quitApp();
+
+private:
+    void setupTray();
+    void applyConfig();
+    void doScreenshot();
+    void doReplayCapture();
+
+    QSystemTrayIcon* m_trayIcon = nullptr;
+    QAction* m_replayToggleAction = nullptr;
+    bool m_replayEnabled = false;
+};
+
+#endif // MAINWINDOW_H
